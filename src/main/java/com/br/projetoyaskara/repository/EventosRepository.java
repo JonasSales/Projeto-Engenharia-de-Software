dfc1f9740d62edbb4a4861507cd9ec16e9d873c5
Jonas <jonassales510@gmail.com>
2025-05-25 17:48:35 -0300

    Implementado novos métodos de busca personalizada.

--- src/main/java/com/br/projetoyaskara/repository/EventosRepository.java
+++ src/main/java/com/br/projetoyaskara/repository/EventosRepository.java
@@ -1,18 +1,25 @@
 package com.br.projetoyaskara.repository;
 
 import com.br.projetoyaskara.model.Eventos;
-import com.br.projetoyaskara.model.Organizacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface EventosRepository extends JpaRepository<Eventos, Long> {
 
-    Eventos findById(long id);
+    Eventos findEventosById(long id);
+
+    @Query("select e from Eventos e where e.organizacao.name like concat('%', :organizacaoName, '%')")
+    List<Eventos> findAllByOrganizacao_Name(@Param("organizacaoName") String organizacaoName);
 
     List<Eventos> findAllByDescricaoContaining(String descricao);
 
-    List<Eventos> findAllByOrganizacao(Organizacao organizacao);
+    List<Eventos> findAllByOrganizacao_Id(UUID organizacaoId);
 
     List<Eventos> findAllByFaixaEtaria(Eventos.FaixaEtaria faixaEtaria);
+
+    List<Eventos> findAllByStatus(Eventos.Status status);
 }
