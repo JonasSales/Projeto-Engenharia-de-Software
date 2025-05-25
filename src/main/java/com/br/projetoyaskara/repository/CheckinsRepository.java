02d073545baf2eb307bd0e5f86fa30745a8b5035
Jonas <jonassales510@gmail.com>
2025-05-25 15:23:53 -0300

    Mini atualização

--- src/main/java/com/br/projetoyaskara/repository/CheckinsRepository.java
+++ src/main/java/com/br/projetoyaskara/repository/CheckinsRepository.java
@@ -1,8 +1,9 @@
 package com.br.projetoyaskara.repository;
 
+import com.br.projetoyaskara.model.Checkins;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Checkins extends JpaRepository<Checkins, Long> {
+public interface CheckinsRepository extends JpaRepository<Checkins, Long> {
 }
