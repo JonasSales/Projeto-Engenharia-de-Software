7d4fccfa3a739c5515bd6a558739121b96fbf865
Jonas <jonassales510@gmail.com>
2025-05-25 16:29:41 -0300

    Implementado do Controller de Orgnaizacao e end point simples para teste.

---
+++ src/main/java/com/br/projetoyaskara/controller/OrganizacaoController.java
@@ -0,0 +1,36 @@
+package com.br.projetoyaskara.controller;
+
+import com.br.projetoyaskara.model.Organizacao;
+import com.br.projetoyaskara.service.OrganizacaoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/organizacao")
+public class OrganizacaoController {
+
+    private final OrganizacaoService organizacaoService;
+
+    public OrganizacaoController(OrganizacaoService organizacaoService) {
+        this.organizacaoService = organizacaoService;
+    }
+
+
+
+    @PostMapping("/create")
+    ResponseEntity<?> registerOrganizacao(@RequestBody Organizacao organizacao) {
+        return organizacaoService.registerOrganization(organizacao);
+    }
+
+    @GetMapping("/get")
+    ResponseEntity<?> getAllOrganizacao() {
+        return organizacaoService.getAllOrganizacoes();
+    }
+
+    @GetMapping("/get/{name}")
+    ResponseEntity<?> getOrganizacaoByName(@PathVariable String name) {
+        return organizacaoService.getOrganizacaoByName(name);
+    }
+
+}
