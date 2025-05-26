f73e469aa89550efac2ff17cd9ba3913419a199a
Jonas <jonassales510@gmail.com>
2025-05-26 16:40:59 -0300

    Atualização em Utils

--- src/main/java/com/br/projetoyaskara/util/Utils.java
+++ src/main/java/com/br/projetoyaskara/util/Utils.java
@@ -3,9 +3,12 @@ package com.br.projetoyaskara.util;
 
 import java.time.LocalDateTime;
 
-public class Utils {
+public final class Utils {
 
+
+    private Utils() {}
     public static LocalDateTime updateDateModified(){
         return LocalDateTime.now();
     }
+
 }
