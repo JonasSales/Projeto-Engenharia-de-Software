e5ce889c1d9a9051f86d370c7af9d6bbac879fb6
Jonas <jonassales510@gmail.com>
2025-05-26 16:40:46 -0300

    Acrescentado EnderecoRepository e adicionado verificações nos metodos.

--- src/main/java/com/br/projetoyaskara/service/EventosService.java
+++ src/main/java/com/br/projetoyaskara/service/EventosService.java
@@ -2,6 +2,7 @@ package com.br.projetoyaskara.service;
 
 import com.br.projetoyaskara.dto.EventosDTO;
 import com.br.projetoyaskara.model.Eventos;
+import com.br.projetoyaskara.repository.EnderecoRepository;
 import com.br.projetoyaskara.repository.EventosRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +11,22 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 
+
 @Service
 public class EventosService {
 
     private final EventosRepository eventosRepository;
+    private final EnderecoRepository enderecoRepository;
 
-    public EventosService(EventosRepository eventosRepository) {
+    public EventosService(EventosRepository eventosRepository, EnderecoRepository enderecoRepository) {
         this.eventosRepository = eventosRepository;
+        this.enderecoRepository = enderecoRepository;
     }
 
     public ResponseEntity<?> cadastrarEvento(Eventos eventos) {
+        if (eventos.getEndereco() != null) {
+            enderecoRepository.save(eventos.getEndereco());
+        }
         return new ResponseEntity<>(eventosRepository.save(eventos), HttpStatus.CREATED);
     }
 
@@ -32,11 +39,18 @@ public class EventosService {
     }
 
     public ResponseEntity<?> buscarEventoPorId(long id) {
-        return ResponseEntity.ok().body(new EventosDTO(eventosRepository.findEventosById(id)));
+        Eventos eventos = eventosRepository.findEventosById(id);
+        if (eventos == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Evento não encontrado");
+        }
+        return ResponseEntity.ok().body(new EventosDTO(eventos));
     }
 
     public ResponseEntity<?> buscarEventosPorNomeDaOrganizacao(String nomeDaOrganizacao) {
         List<Eventos> eventos = eventosRepository.findAllByOrganizacao_Name(nomeDaOrganizacao);
+        if (eventos == null || eventos.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não existe eventos com o nome dessa organização");
+        }
         List<EventosDTO> dtos = eventos.stream()
                 .map(EventosDTO::new)
                 .toList();
@@ -46,6 +60,9 @@ public class EventosService {
 
     public ResponseEntity<?> buscarEventosPorDescricao(String descricao) {
         List<Eventos> eventos = eventosRepository.findAllByDescricaoContaining(descricao);
+        if (eventos == null || eventos.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum evento com essa descricao");
+        }
         List<EventosDTO> dtos = eventos.stream()
                 .map(EventosDTO::new)
                 .toList();
@@ -53,27 +70,52 @@ public class EventosService {
     }
 
     public ResponseEntity<?> buscarEventosPorOrganizacaoId(UUID organizacaoId) {
-        List<Eventos> eventos = eventosRepository.findAllByOrganizacao_Id(organizacaoId);
-        List<EventosDTO> dtos = eventos.stream()
-                .map(EventosDTO::new)
-                .toList();
-        return ResponseEntity.ok(dtos);
+        try {
+            List<Eventos> eventos = eventosRepository.findAllByOrganizacao_Id(organizacaoId);
+            if (eventos == null || eventos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não existe eventos, vefique o ID da organização");
+            }
+            List<EventosDTO> dtos = eventos.stream()
+                    .map(EventosDTO::new)
+                    .toList();
+            return ResponseEntity.ok(dtos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado");
+        }
+
     }
 
     public ResponseEntity<?> buscarEventosPorFaixaEtaria(String faixaEtaria) {
-        List<Eventos> eventos = eventosRepository.findAllByFaixaEtaria(Eventos.FaixaEtaria.valueOf(faixaEtaria));
-        List<EventosDTO> dtos = eventos.stream()
-                .map(EventosDTO::new)
-                .toList();
-        return ResponseEntity.ok(dtos);
+        try {
+            Eventos.FaixaEtaria faixa = Eventos.FaixaEtaria.valueOf(faixaEtaria);
+            List<Eventos> eventos = eventosRepository.findAllByFaixaEtaria(faixa);
+            if (eventos == null || eventos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não há eventos com essa faixa etária");
+            }
+            List<EventosDTO> dtos = eventos.stream()
+                    .map(EventosDTO::new)
+                    .toList();
+            return ResponseEntity.ok(dtos);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Inconsistência na busca por faixa  etária");
+        }
     }
 
     public ResponseEntity<?> buscarEventosPorStatus(String status) {
-        List<Eventos> eventos = eventosRepository.findAllByStatus(Eventos.Status.valueOf(status));
-        List<EventosDTO> dtos = eventos.stream()
-                .map(EventosDTO::new)
-                .toList();
-        return ResponseEntity.ok(dtos);
+        try{
+            Eventos.Status sta = Eventos.Status.valueOf(status);
+            List<Eventos> eventos = eventosRepository.findAllByStatus(sta);
+            if  (eventos == null || eventos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não há eventos com esse status");
+            }
+            List<EventosDTO> dtos = eventos.stream()
+                    .map(EventosDTO::new)
+                    .toList();
+            return ResponseEntity.ok(dtos);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Inconsistência na busca por status");
+        }
+
     }
 
 }
