96cdd02137fa0e433dfc637a302ad168eb28adbc
Jonas <jonassales510@gmail.com>
2025-05-26 18:22:15 -0300

    Adicionado método para atualização de Endereço.

--- src/main/java/com/br/projetoyaskara/util/Utils.java
+++ src/main/java/com/br/projetoyaskara/util/Utils.java
@@ -1,6 +1,8 @@
 package com.br.projetoyaskara.util;
 
 
+import com.br.projetoyaskara.model.Endereco;
+
 import java.time.LocalDateTime;
 
 public final class Utils {
@@ -10,5 +12,14 @@ public final class Utils {
     public static LocalDateTime updateDateModified(){
         return LocalDateTime.now();
     }
+
+    public static void atualizarEndereco(Endereco destino, Endereco origem) {
+        destino.setBairro(origem.getBairro());
+        destino.setCep(origem.getCep());
+        destino.setEstado(origem.getEstado());
+        destino.setComplemento(origem.getComplemento());
+        destino.setCidade(origem.getCidade());
+    }
+
 
 }
