11c3ca4188342b07ad3637ff2f6476e9efa422cd
Jonas <jonassales510@gmail.com>
2025-05-26 16:36:48 -0300

    Criação de OrganizaçãoDTO  para transfêrencia de dados na busca.

---
+++ src/main/java/com/br/projetoyaskara/dto/OrganizacaoDTO.java
@@ -0,0 +1,33 @@
+package com.br.projetoyaskara.dto;
+
+
+import com.br.projetoyaskara.model.Organizacao;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class OrganizacaoDTO {
+
+    public OrganizacaoDTO(Organizacao organizacao) {
+        setId(organizacao.getId());
+        setName(organizacao.getName());
+        setDescricao(organizacao.getDescription());
+        setEndereco(new EnderecoDTO(organizacao.getEndereco()));
+        setCnpj(organizacao.getCnpj());
+    }
+
+    private UUID id;
+    private String name;
+    private String descricao;
+    private String cnpj;
+    private EnderecoDTO endereco;
+
+}
