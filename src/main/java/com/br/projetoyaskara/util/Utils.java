d9734054a5b227f4c02ae18f599c48425455ac3c
Jonas <jonassales510@gmail.com>
2025-05-26 18:52:04 -0300

    Remoção de função sem utilização.

--- src/main/java/com/br/projetoyaskara/util/Utils.java
+++ src/main/java/com/br/projetoyaskara/util/Utils.java
@@ -3,15 +3,11 @@ package com.br.projetoyaskara.util;
 
 import com.br.projetoyaskara.model.Endereco;
 
-import java.time.LocalDateTime;
 
 public final class Utils {
 
 
     private Utils() {}
-    public static LocalDateTime updateDateModified(){
-        return LocalDateTime.now();
-    }
 
     public static void atualizarEndereco(Endereco destino, Endereco origem) {
         destino.setBairro(origem.getBairro());
