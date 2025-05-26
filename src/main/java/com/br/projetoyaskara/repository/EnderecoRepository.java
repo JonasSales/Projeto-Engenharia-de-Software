c9418b674ecfba67c8411d58babd413db47b33a9
Jonas <jonassales510@gmail.com>
2025-05-26 18:22:47 -0300

    Alteração de nome para melhor entedimento de função.

--- src/main/java/com/br/projetoyaskara/repository/EnderecoRepository.java
+++ src/main/java/com/br/projetoyaskara/repository/EnderecoRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
-    Endereco findById(long id);
+    Endereco findEnderecoById(long id);
 
 }
