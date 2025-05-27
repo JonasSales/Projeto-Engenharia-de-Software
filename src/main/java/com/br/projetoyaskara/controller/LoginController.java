e338552400547f8445cb062607c3f679d7d1a3a5
Jonas <jonassales510@gmail.com>
2025-05-27 08:52:50 -0300

    Adicionado captura de exceções e erros

--- src/main/java/com/br/projetoyaskara/controller/LoginController.java
+++ src/main/java/com/br/projetoyaskara/controller/LoginController.java
@@ -5,9 +5,12 @@ import com.br.projetoyaskara.dto.AuthenticationRequest;
 import com.br.projetoyaskara.dto.AuthenticationResponse;
 import com.br.projetoyaskara.model.ClientUser;
 import com.br.projetoyaskara.service.TokenService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,13 +30,24 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest) {
-        var userNamePassword = new UsernamePasswordAuthenticationToken(
-                authenticationRequest.email(),
-                authenticationRequest.password()
-        );
+        try {
+            var userNamePassword = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.email(),
+                    authenticationRequest.password()
+            );
 
-        var auth = authenticationManager.authenticate(userNamePassword);
-        var token = tokenService.generateToken((ClientUser) auth.getPrincipal());
-        return ResponseEntity.ok(new AuthenticationResponse(token));
+            var auth = authenticationManager.authenticate(userNamePassword);
+            var token = tokenService.generateToken((ClientUser) auth.getPrincipal());
+
+            return ResponseEntity.ok(new AuthenticationResponse(token));
+
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Senha incorreta");
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao autenticar");
+        }
     }
+
 }
