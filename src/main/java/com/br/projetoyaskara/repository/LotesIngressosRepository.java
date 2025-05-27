d7092a67dcbe3ac2988f761407b9a9102a2ca18f
Jonas <jonassales510@gmail.com>
2025-05-27 09:33:23 -0300

    Adicionado métodos de busca por Id de Ingresso e Ingressos por evento, como também buscar ingressos por faixa de valor.

--- src/main/java/com/br/projetoyaskara/repository/LotesIngressosRepository.java
+++ src/main/java/com/br/projetoyaskara/repository/LotesIngressosRepository.java
@@ -2,8 +2,20 @@ package com.br.projetoyaskara.repository;
 
 import com.br.projetoyaskara.model.LotesIngresso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LotesIngressosRepository extends JpaRepository<LotesIngresso, Long> {
+
+    LotesIngresso findLotesIngressoById(Long id);
+
+    LotesIngresso findLotesIngressoByEventoId(Long eventoId);
+
+    @Query("select e from LotesIngresso e where ((e.valor > :primeiraFaixa) and (e.valor < :segundaFaixa)) ")
+    List<LotesIngresso> findLotesIngressosPorFaixaDePreco(@Param("primeiraFaixa") int primeiraFaixa,
+                                                          @Param("segundaFaixa") int segundaFaixa);
 }
