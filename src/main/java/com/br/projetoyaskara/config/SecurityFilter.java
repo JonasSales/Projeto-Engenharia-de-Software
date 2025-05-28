0d1c8bd981d6b13da65c3525fe36e72f3883a45d
Jonas <jonassales510@gmail.com>
2025-05-28 18:31:52 -0300

    Adicionado anotação @NonNull para remoção de warnings

--- src/main/java/com/br/projetoyaskara/config/SecurityFilter.java
+++ src/main/java/com/br/projetoyaskara/config/SecurityFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,9 +32,9 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
             String token = this.recoverToken(request);
             if (token != null) {
