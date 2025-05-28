8ba25ad7e4f52a51c3de3e01996a5cfe828a738c
Jonas <jonassales510@gmail.com>
2025-05-28 16:50:46 -0300

    Remoção de função sem uso.

--- src/main/java/com/br/projetoyaskara/exception/TokenException.java
+++ src/main/java/com/br/projetoyaskara/exception/TokenException.java
@@ -2,9 +2,6 @@ package com.br.projetoyaskara.exception;
 
 public class TokenException extends RuntimeException {
 
-    public TokenException(String message) {
-        super(message);
-    }
 
     public TokenException(String message, Throwable cause) {
         super(message, cause);
