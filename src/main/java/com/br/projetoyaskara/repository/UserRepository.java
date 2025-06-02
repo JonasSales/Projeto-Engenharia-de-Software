869316fcc821e08825fcfaa9d86d2f78c40d2ac8
Jonas <jonassales510@gmail.com>
2025-06-02 19:58:09 -0300

    Adicionado novos métodos personalizados no UserRepository

--- src/main/java/com/br/projetoyaskara/repository/UserRepository.java
+++ src/main/java/com/br/projetoyaskara/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.br.projetoyaskara.repository;
 
 import com.br.projetoyaskara.model.ClientUser;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -16,4 +18,10 @@ public interface UserRepository extends CrudRepository<ClientUser, UUID> {
 
     ClientUser findClientUserById(UUID id);
 
+    @Query("SELECT COUNT(u) > 0 FROM ClientUser u WHERE u.email = :email")
+    boolean existsByEmail(@Param("email") String email);
+
+    @Modifying
+    @Query("DELETE FROM ClientUser c WHERE c.email = :email")
+    void deleteClientUserByEmail(@Param("email") String email);
 }
