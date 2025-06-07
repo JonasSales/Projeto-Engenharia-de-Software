be0afa4c33735b60e1e43781a9767b00309c717c
Jonas <jonassales510@gmail.com>
2025-06-07 15:36:58 -0300

    Remoção de algumas consultas ao Banco de Dados.

--- src/main/java/com/br/projetoyaskara/repository/UserRepository.java
+++ src/main/java/com/br/projetoyaskara/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.br.projetoyaskara.repository;
 
 import com.br.projetoyaskara.model.ClientUser;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -16,12 +15,7 @@ public interface UserRepository extends CrudRepository<ClientUser, UUID> {
 
     ClientUser findByToken(String token);
 
-    ClientUser findClientUserById(UUID id);
-
     @Query("SELECT COUNT(u) > 0 FROM ClientUser u WHERE u.email = :email")
     boolean existsByEmail(@Param("email") String email);
 
-    @Modifying
-    @Query("DELETE FROM ClientUser c WHERE c.email = :email")
-    void deleteClientUserByEmail(@Param("email") String email);
 }
