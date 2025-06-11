391d48357643a0223b1b219bc45322006221d21b
Jonas <jonassales510@gmail.com>
2025-06-11 12:00:27 -0300

    Mapeando o ID

--- src/main/java/com/br/projetoyaskara/mapper/EventosMapper.java
+++ src/main/java/com/br/projetoyaskara/mapper/EventosMapper.java
@@ -3,15 +3,17 @@ package com.br.projetoyaskara.mapper;
 import com.br.projetoyaskara.dto.EventosDTO;
 import com.br.projetoyaskara.model.Eventos;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EventosMapper {
 
-    EventosDTO eventosToEventosDTO(Eventos eventos);
+    @Mapping(source = "organizacao.id", target = "organizacaoId")
+    EventosDTO toDTO(Eventos eventos);
 
-    List<EventosDTO> eventosToEventosDTO(List<Eventos> eventos);
+    List<EventosDTO> toDTO(List<Eventos> eventos);
 
-    Eventos eventosDTOToEventos(EventosDTO eventosDTO);
+    Eventos toEntity(EventosDTO eventosDTO);
 }
