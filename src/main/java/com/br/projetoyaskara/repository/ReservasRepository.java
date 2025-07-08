da26cf61fc53b8c5936cc2a8222c92b4a4ec4ef0
Jonas <jonassales510@gmail.com>
2025-07-08 19:09:03 -0300

    Alteração em Repository

--- src/main/java/com/br/projetoyaskara/repository/ReservasRepository.java
+++ src/main/java/com/br/projetoyaskara/repository/ReservasRepository.java
@@ -4,7 +4,6 @@ import com.br.projetoyaskara.model.Reservas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,7 +11,5 @@ import java.util.UUID;
 public interface ReservasRepository extends JpaRepository<Reservas, Long> {
 
     List<Reservas> findReservasByClientId(UUID clienteId);
-
-    List<Reservas> findReservasByDataReservaBeforeAndClient_Id(LocalDateTime dataReservaBefore, UUID clientId);
 
 }
