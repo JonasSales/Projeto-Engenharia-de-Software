3e6da25f2e126fe8b8189709c51684cb7cc53679
Jonas <jonassales510@gmail.com>
2025-07-16 18:20:52 -0300

    Alterando para class

--- src/main/java/com/br/projetoyaskara/dto/response/ItemPedidoResponseDTO.java
+++ src/main/java/com/br/projetoyaskara/dto/response/ItemPedidoResponseDTO.java
@@ -1,14 +1,30 @@
 package com.br.projetoyaskara.dto.response;
 
-import jakarta.validation.constraints.NotNull;
+import com.br.projetoyaskara.model.ItemPedido;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.UUID;
 
-public record ItemPedidoResponseDTO(
-        @NotNull UUID id,
-        @NotNull UUID pedidoId,
-        @NotNull UUID loteIngressoId,
-        @NotNull String nomeIngresso,
-        int valor
-) {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPedidoResponseDTO{
+    private UUID id;
+    private UUID pedidoId;
+    private UUID loteIngressoId;
+    private String nomeIngresso;
+    private int valo;
 
+    public ItemPedidoResponseDTO(ItemPedido itemPedido){
+        this.id = itemPedido.getId();
+        this.pedidoId = itemPedido.getPedido().getId();
+        this.loteIngressoId = itemPedido.getLotesIngresso().getId();
+        this.nomeIngresso = itemPedido.getNomeIngresso();
+        this.valo = itemPedido.getValor();
+    }
 }
+
