488ca9eeccc3e59db173b9f43757beb1a023e83a
Jonas <jonassales510@gmail.com>
2025-07-16 08:51:53 -0300

    Criação de DTO's de response e request para melhor organização de código.

--- src/main/java/com/br/projetoyaskara/dto/request/EventoCreateRequestDTO.java
+++ src/main/java/com/br/projetoyaskara/dto/request/EventoCreateRequestDTO.java
@@ -1,15 +1,12 @@
-package com.br.projetoyaskara.dto;
+package com.br.projetoyaskara.dto.request;
 
-import com.br.projetoyaskara.model.Endereco;
-import com.br.projetoyaskara.model.Eventos;
+import com.br.projetoyaskara.model.Eventos; // Supondo que os Enums estejam aqui
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -17,30 +14,32 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class EventosDTO {
 
-    private Long id;
+public class EventoCreateRequestDTO {
+
     @NotBlank(message = "Nome do evento é obrigatório.")
-    @Size(max = 255, message = "Nome do evento não pode exceder 255 caracteres.")
+    @Size(max = 255)
     private String name;
+
     @Size(max = 1000, message = "Descrição do evento não pode exceder 1000 caracteres.")
     private String descricao;
+
     @NotNull(message = "Data de início é obrigatória.")
     @FutureOrPresent(message = "Data de início não pode ser no passado.")
     private LocalDateTime dataInicio;
+
     @NotNull(message = "Data de fim é obrigatória.")
     @FutureOrPresent(message = "Data de fim não pode ser no passado.")
     private LocalDateTime dataFim;
+
     @NotNull(message = "Faixa etária é obrigatória.")
     private Eventos.FaixaEtaria faixaEtaria;
-    @NotNull(message = "Status é obrigatório.")
-    private Eventos.Status status;
+
     @NotNull(message = "Organização é obrigatória.")
     private UUID organizacaoId;
-    @Valid
 
-    private Endereco endereco;
+    //@NotNull(message = "Endereço é obrigatório.")
+    @Valid
+    private EnderecoRequestDTO endereco;
 
 }
